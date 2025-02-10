da3f05810f21cb5e14894bbb3c074fdff508748f
KILI-BC <45309327+KILI-BC@users.noreply.github.com>
2025-02-10 22:50:09 +0100

    Started implementation of functions
    - Changed structure of headder-file
    - Implemented create-functions

--- src/enigma.c
+++ src/enigma.c
@@ -1,4 +1,7 @@
 #include "enigma.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 static int roll_in_alphabet(int i, int shift, int alphabet_size)
 {
@@ -13,3 +16,139 @@ static int roll_in_alphabet(int i, int shift, int alphabet_size)
     return i % alphabet_size;
 }
 
+rotor *rotor_create(const char number[], int ring_setting)
+{
+	rotor *r_ptr;
+	int rotor_number, i;
+	char* number_cpy;
+    /*check ring setting*/
+    if(ring_setting < 0 || ring_setting > 25)
+        return NULL;
+
+    /*copy number and convert to lower*/
+    number_cpy = malloc(sizeof(number));
+	if(number_cpy == NULL)
+        return NULL;
+    strcpy(number_cpy, number);
+    for (i = 0; i < strlen(number_cpy); i++)
+        number_cpy[i] = tolower(number_cpy[i]);
+
+    /*create rotor*/
+    r_ptr = malloc(sizeof(rotor));
+    if(r_ptr == NULL){
+        free(number_cpy);
+        return NULL;
+    }
+
+    r_ptr->ring_setting = ring_setting;
+    r_ptr->turnover_markers[1] = -1;
+    /*set key and ring setting depending on used rotor*/
+    if(strcmp(number_cpy, "i")){
+        r_ptr = key_create("ekmflgdqvzntowyhxuspaibrcj");
+        r_ptr->turnover_markers[0] = "q" - "a";
+    } else if(strcmp(number_cpy, "ii")){
+        r_ptr = key_create("ajdksiruxblhwtmcqgznpyfvoe");
+        r_ptr->turnover_markers[0] = "e" - "a";
+    } else if(strcmp(number_cpy, "iii")){
+        r_ptr = key_create("bdfhjlcprtxvznyeiwgakmusqo");
+        r_ptr->turnover_markers[0] = "v" - "a";
+    } else if(strcmp(number_cpy, "iv")){
+        r_ptr = key_create("esovpzjayquirhxlnftgkdcmwb");
+        r_ptr->turnover_markers[0] = "j" - "a";
+    } else if(strcmp(number_cpy, "v")){
+        r_ptr = key_create("vzbrgityupsdnhlxawmjqofeck");
+        r_ptr->turnover_markers[0] = "z" - "a";
+    } else if(strcmp(number_cpy, "vi")){
+        r_ptr = key_create("jpgvoumfyqbenhzrdkasxlictw");
+        r_ptr->turnover_markers[0] = "z" - "a";
+        r_ptr->turnover_markers[1] = "m" - "a";
+    } else if(strcmp(number_cpy, "vii")){
+        r_ptr = key_create("nzjhgrcxmyswboufaivlpekqdt");
+        r_ptr->turnover_markers[0] = "z" - "a";
+        r_ptr->turnover_markers[1] = "m" - "a";
+    } else if(strcmp(number_cpy, "viii")){
+        r_ptr = key_create("fkqhtlxocbjspdzramewniuygv");
+        r_ptr->turnover_markers[0] = "z" - "a";
+        r_ptr->turnover_markers[1] = "m" - "a";
+    } else {
+        free(number_cpy);
+        free(r_ptr);
+        return NULL;
+    }
+
+    /*free in case of unsuccessful memory allocation for key*/
+    if(r_ptr->k == NULL){
+        free(number_cpy);
+        free(r_ptr);
+        return NULL;
+    }
+
+    return r_ptr;
+}
+
+reflector *reflector_create(const char type)
+{
+    rotor *r_ptr;
+	int rotor_number, i;
+    char type_lower = tolower(type);
+    /*create reflector*/
+    r_ptr = malloc(sizeof(reflector));
+    if(r_ptr == NULL)
+        return NULL;
+
+    /*set key depending on used reflector*/
+    switch (tolower(type))
+    {
+    case 'a':
+        r_ptr->k = key_create("ejmzalyxvbwfcrquontspikhgd");
+        break;
+    case 'b':
+        r_ptr->k = key_create("yruhqsldpxngokmiebfzcwvjat");
+        break;
+    case 'c':
+        r_ptr->k = key_create("fvpjiaoyedrzxwgctkuqsbnmhl");
+        break;
+    default:
+        free(r_ptr);
+        return NULL;
+    }
+    /*free in case of unsuccessful memory allocation for key*/
+    if(r_ptr->k == NULL){
+        free(r_ptr);
+        return NULL;
+    }
+
+    return r_ptr;
+}
+
+plugboard *plugboard_create(const char key_str[])
+{
+    plugboard *p_ptr = malloc(sizeof(plugboard));
+    if(p_ptr == NULL)
+        return NULL;
+    p_ptr->k = key_create(key_str);
+    if(p_ptr->k == NULL){
+        free(p_ptr);
+        return NULL;
+    }
+    return p_ptr;
+}
+
+enigma *enigma_create(rotor *rotor_left, rotor *rotor_middle, rotor *rotor_right, reflector *r, plugboard *p)
+{
+    enigma *e_ptr;
+    if(rotor_left == NULL || rotor_middle == NULL || rotor_right == NULL || r == NULL || p == NULL)
+        return NULL;
+
+    e_ptr = malloc(sizeof(enigma));
+    if(e_ptr == NULL)
+        return NULL;
+
+    e_ptr->rotor_left = rotor_left;
+    e_ptr->rotor_middle = rotor_middle;
+    e_ptr->rotor_right = rotor_right;
+    e_ptr->reflector = r;
+    e_ptr->plugboard = p;
+
+    return e_ptr;
+}
--- src/enigma.h
+++ src/enigma.h
@@ -1,20 +1,20 @@
 #ifndef ENIGMA_H_INCLUDED
 #define ENIGMA_H_INCLUDED
 
+typedef int key[26];
+
 typedef struct _reflector {
-    void *key;
+    key *k;
 } reflector;
 
 typedef struct _rotor {
-    char turnover_markers[2];
-    int ringstellung;
-    void *key;
+    int turnover_markers[2];
+    int ring_setting;
+    key *k;
 } rotor;
 
 typedef struct _plugboard {
-    char turnover_markers[2];
-    int ringstellung;
-    void *key;
+    key *k;
 } plugboard;
 
 typedef struct _enigma {
@@ -22,7 +22,7 @@ typedef struct _enigma {
     rotor *rotor_left;
     rotor *rotor_middle;
     rotor *rotor_right;
-    void *plugboard_key;
+    plugboard *plugboard;
     int rotor_positions[3];
 } enigma;
 
@@ -30,16 +30,27 @@ static int roll_in_alphabet(int i, int shift, int alphabet_size);
 
 rotor *rotor_create(const char number[], int ringstellung);
 reflector *reflector_create(const char type);
-plugboard *plugboard_create(const char key[]);
-enigma *enigma_create(rotor rotor_left, rotor rotor_middle, rotor rotor_right, reflector r, plugboard p);
+plugboard *plugboard_create(const char key_str[]);
+enigma *enigma_create(rotor *rotor_left, rotor *rotor_middle, rotor *rotor_right, reflector *r, plugboard *p);
+key *key_create(const char key_str[]);
 
 void rotor_destroy(rotor *r);
 void reflector_destroy(reflector *r);
 void plugboard_destroy(plugboard *p);
 void enigma_destroy(enigma *e);
+void key_destroy(key *k);
+
+void rotor_check(rotor *r);
+void reflector_check(reflector *r);
+void plugboard_check(plugboard *p);
+int enigma_check(enigma *e);
+int key_check(key *k);
+
+static void key_encrypt(key *k, int i);
+static void key_decrypt(key *k, int i);
 
 /*changes structs!*/
-void encrypt(enigma *e, char *text);
-void rotate_rotor(rotor *r);
+void enigma_encrypt(enigma *e, char *text);
+void rotor_rotate(rotor *r);
 
 #endif
